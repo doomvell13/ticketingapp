9eeeb7cf9e53babaeeb8d30f71ba0a3d871d3275
Anthony Ong <wknight13@gmail.com>
2022-03-18 21:16:33 +0800

    change build client return url

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,14 +5,13 @@ export default ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.ticketing-app-project.xyz/',
       headers: req.headers,
     });
   } else {
     // We must be on the browser
     return axios.create({
-      baseUrl: 'http://www.ticketing-app-project.xyz',
+      baseUrl: '/',
     });
   }
 };
